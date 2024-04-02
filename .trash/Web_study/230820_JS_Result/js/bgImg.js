178e61e67e4a72815aa78778812b385760d5b501
odineyes2 <55679108+odineyes2@users.noreply.github.com>
2024-04-02 22:25:37 +0900

    240402-5

--- .trash/Web_study/230820_JS_Result/js/bgImg.js
+++
@@ -1,8 +0,0 @@
-const ImgList = ["0.jpeg", "1.jpeg", "3.jpeg"];
-const ImgNo = Math.floor(Math.random() * ImgList.length);
-const todayImg = ImgList[ImgNo];
-
-const bgImg = document.createElement("img");
-bgImg.src = `img/${todayImg}`;
-// console.log(bgImg);
-document.body.appendChild(bgImg);
--- .trash/Web_study/js/bgImg.js
+++
@@ -1,6 +0,0 @@
-const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
-const img = document.createElement("img");
-document.body.appendChild(img);
-const todayImgNo = Math.floor(Math.random() * images.length);
-const todayImg = images[todayImgNo];
-img.src = `img/${todayImg}`;
